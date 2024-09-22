805a824584a08750e1fdf88245659976b677add6
Blacky Wolf <support@blackywolf.me>
2024-09-21 21:22:02 -0500

    Backing up SQL so I don't have to re-input this manually

---
+++ backups/trees_202409212120.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.trees ("name",description,public,user_id) VALUES
+	 ('Lineage of Adam','The lineage of Adam and Eve, as defined as correctly as possible from the KJV bible.',true,'96c2f247-1b2f-4821-a780-e0a8f5c3f4e5');
